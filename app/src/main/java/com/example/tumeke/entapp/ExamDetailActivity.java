408675b9a7e4332b3d4a9d11581a8bad35dafc21
Tumeke94 <tumarkhan.yesboluly@gmail.com>
2017-06-17 15:38:09 +0600

    firebase List View

--- app/src/main/java/com/example/tumeke/entapp/ExamDetailActivity.java
+++ app/src/main/java/com/example/tumeke/entapp/ExamDetailActivity.java
@@ -3,18 +3,14 @@ package com.example.tumeke.entapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,16 +33,30 @@ public class ExamDetailActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         database = FirebaseDatabase.getInstance();
-        myRef = database.getReference();
+        myRef = database.getReference("demo");
 
         questions = new ArrayList<>();
 
-        myRef.addValueEventListener(new ValueEventListener() {
+        myRef.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    questions.add(snapshot.getValue().toString());
-                }
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Toast.makeText(getApplicationContext(), dataSnapshot.getValue(String.class), Toast.LENGTH_SHORT);
+                //questions.add(dataSnapshot.getValue(String.class));
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
             }
 
             @Override
@@ -54,9 +64,10 @@ public class ExamDetailActivity extends AppCompatActivity {
 
             }
         });
+
         questionTV = (TextView) findViewById(R.id.questionTV);
-        questionTV.setText(questions.get(0));
-        if(!questions.isEmpty() && questions.size() > 0) {
+        //questionTV.setText(questions.get(0));
+        if (!questions.isEmpty() && questions.size() > 0) {
             questionTV.setText(questions.get(0));
         } else {
             Toast.makeText(getApplicationContext(), "Empty List", Toast.LENGTH_SHORT);
